f845906efed9ca9289e1608aca6603e2c112f5a9
Nabor <Nabor@DESKTOP-BTR17DU>
2016-06-06 09:51:32 +0200

    Añadidos comentarios sobre el funcionamiento del bucle for y sobre la condición if

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -20,7 +20,12 @@ public class Principal {
         if (dig <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        
+        /**
+         * Bucle que recorrerá la variable int i mientras sea menor o igual a 99999, cada vuelta de bucle sumará al contador i++.
+         * La condición while dentro del bucle marca que cuando  variable aux sea distinta a 0 realice la operacion aux/10, asigne el valor a aux y añada una vuelta al contador.
+         * @param aux Variable de tipo int que le es asginado como valor la variable i.
+         * @param contador Variable de tipo int inicializada en 0.
+         */
         for (int i = 1; i <= 99999; i++) {
             int aux = i;
 
@@ -30,6 +35,11 @@ public class Principal {
                 aux = aux / 10;
                 contador++;
             }
+        /**
+         * Se le asigna a la variable ndig el valor de la variable contador.
+         * Ahora se usará la variable ndig como la variable contador.
+         * A partir de este punto comienzan las condiciones que ha de pasar el digito introducido para ver si es mostrado o no.
+         */
             ndig = contador;
 
             if (ndig == dig) {
